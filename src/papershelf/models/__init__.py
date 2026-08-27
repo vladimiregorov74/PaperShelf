94cc4d9377227594652223f714235ae310cc98b5
vladimiregorov74 <vladimiregorov74@gmail.com>
2026-08-27 18:57:59 +0300

    Добавил README.md

--- src/papershelf/models/__init__.py
+++ src/papershelf/models/__init__.py
@@ -1,13 +1,11 @@
 from .article import Article
 from .library_item import LibraryItem
 from .supported_site import SupportedSite
-from .library_statistics import LibraryStatistics
 
-__all__ = [
+__all__ = (
     "Article",
     "LibraryItem",
 	"SupportedSite",
-    "LibraryStatistics",
-]
+)
 
 
